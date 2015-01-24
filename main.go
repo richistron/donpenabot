0ddd8869de76abed1957c764fe59e32c1175126d
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2015-01-24 16:32:02 -0600

    jwt auth

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"net/url"
 	"os"
 	"os/signal"
 	"time"
 
+	"code.google.com/p/goauth2/oauth/jwt"
+
+	prediction "code.google.com/p/google-api-go-client/prediction/v1.6"
 	"github.com/ChimeraCoder/anaconda"
 )
 
@@ -27,6 +31,20 @@ func main() {
 		"Como ya lo ha comentado el presidente de Francia, los temas tratados fueron varios, abordando distintos temas",
 	}
 	api := initializeAPI()
+
+	token := createToken()
+
+	if transport, err := jwt.NewTransport(token); err == nil {
+		service, err := prediction.New(transport.Client())
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		models := service.Trainedmodels.List("premium-ember-835")
+		fmt.Printf("%#v\n", models)
+
+	}
+
 	go tweet(api, frases)
 	// Set up channel on which to send signal notifications.
 	// We must use a buffered channel or risk missing the signal
@@ -65,4 +83,18 @@ func tweet(api *anaconda.TwitterApi, frases []string) {
 func randint(length int) int {
 	rand.Seed(time.Now().UTC().UnixNano())
 	return rand.Intn(length)
+}
+
+func createToken() *jwt.Token {
+	// Craft the ClaimSet and JWT token.
+	pemKeyBytes, err := ioutil.ReadFile("pkey.pem")
+	if err != nil {
+		panic(err)
+	}
+
+	iss := "621740100651-7lkhc3notki0lce8ps60tf2ddk8je1p7@developer.gserviceaccount.com"
+	scope := "https://www.googleapis.com/auth/prediction"
+	t := jwt.NewToken(iss, scope, pemKeyBytes)
+
+	return t
 }
